c6768431c1dbea9d96f46e77e356f5a798cddeff
DmitryDemid <dimon.demid15@gmail.com>
2021-11-05 11:07:22 -0400

    refactoring

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlugin(activeSlide = 0) {
+    const slides = document.querySelectorAll('.slide');
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
+    slides[activeSlide].classList.add('active')
 
-       slide.classList.add('active')
-    });
-}
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
 
-const clearActiveClasses = () => slides.forEach(slide => slide.classList.remove('active'))
+            slide.classList.add('active')
+        });
+    };
+
+    const clearActiveClasses = () => slides.forEach(slide => slide.classList.remove('active'));
+};
+
+slidesPlugin()
